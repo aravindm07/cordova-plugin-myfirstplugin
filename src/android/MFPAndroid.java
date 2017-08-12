bb78d6a152b0d426ae167e9821424bf00e2b30dc
aravindm07 <30870223+aravindm07@users.noreply.github.com>
2017-08-12 18:41:02 +0530

    Update MFPAndroid.java

--- src/android/MFPAndroid.java
+++ src/android/MFPAndroid.java
@@ -4,16 +4,16 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-public class MFPAndroid extends CordovaPlugin {
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("greet")) {
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
+// public class MFPAndroid extends CordovaPlugin {
+//     @Override
+//     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+//         if (action.equals("greet")) {
+//             String name = data.getString(0);
+//             String message = "Hello, " + name;
+//             callbackContext.success(message);
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
